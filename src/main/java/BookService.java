765eb472a8a13422ca05a15ca4969bd8ee3278e2
Kola Ra <santaramus@gmail.com>
2019-10-05 14:59:10 +0300

    BookServices.java done

--- src/main/java/BookService.java
+++ src/main/java/BookService.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class BookService {
+    public static void printElementsByAauthor(String autor, ArrayList<Book> arrayToPrint) {
+        for (Book element : arrayToPrint){
+            if ( autor == element.getAuthor())
+                System.out.println(element.toString());
+        }
+    }
+
+    public static void printElementsByPublisher(String publisher, ArrayList<Book> arrayToPrint) {
+        for (Book element : arrayToPrint){
+            if ( publisher == element.getPublisher())
+                System.out.println(element.toString());
+        }
+
+    }
+
+    public static void printElementsByStorageLife(int storageLife, ArrayList<Book> arrayToPrint){
+        for (Book element : arrayToPrint){
+            if (storageLife > element.getYearOfPublish()){
+                System.out.println(element.toString());
+            }
+        }
+    }
+
 
 }
